7d7ac6b26ab4da7c044d9ecaccda022ee113bccd
NutanSH <nutanhegde.sv@gmail.com>
2020-05-11 01:11:13 +0530

    fix: Issue Fix for Non-owner of the image being able to edit the image

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -92,14 +92,25 @@ public class ImageController {
 
     //The method first needs to convert the list of all the tags to a string containing all the tags separated by a comma and then add this string in a Model type object
     //This string is then displayed by 'edit.html' file as previous tags of an image
+    //Issue Fix for Non-owner of the image able to edit the image, editImage method is modified to check the logged in User against the owner of the Image
     @RequestMapping(value = "/editImage")
-    public String editImage(@RequestParam("imageId") Integer imageId, Model model) {
+    public String editImage(@RequestParam("imageId") Integer imageId, HttpSession session, Model model) {
+
+        String error = "Only the owner of the image can edit the image";
         Image image = imageService.getImage(imageId);
 
         String tags = convertTagsToString(image.getTags());
         model.addAttribute("image", image);
         model.addAttribute("tags", tags);
-        return "images/edit";
+
+        User user = (User) session.getAttribute("loggeduser");
+        if (image.getUser().getId() == user.getId())
+            return "images/edit";
+
+        else {
+            model.addAttribute("editError", error);
+            return "images/image";
+        }
     }
 
     //This controller method is called when the request pattern is of type 'images/edit' and also the incoming request is of PUT type
